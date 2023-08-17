b635d47d09db464ab34cb7f138d3e6f224ac51d5
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2023-08-17 15:37:11 +0000

    New Style

--- reflex/src/App.js
+++ reflex/src/App.js
@@ -9,22 +9,30 @@ import YouTubeMusicPlayer from './YouTubeMusicPlayer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="grid-container">
-          <div className="grid-item">
-            <div className='github'><WeatherWidget/></div>
-          </div>
-          <div className="grid-item">
-            <div className='clock'><Clock/></div>
-          </div>
-          <div className="grid-item">
-            <div className='caption'><Captioning/></div>
-          </div>
-          <div className="grid-item">
-            <div className='lofi'><LofiPlayer/></div>
-          </div>
-        </div>
-      </header>
+      <div class="container">
+    <header class="header">
+      <h1>Scroller Blog</h1>
+    </header>
+    
+    <main>
+      <article class="post">
+        <h2 class="post-title">Post Title 1</h2>
+        <p class="post-content">This is the content of the first post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit, nisi in facilisis cursus, neque ex tincidunt arcu, eu vulputate libero justo id ex.</p>
+      </article>
+      
+      <article class="post">
+        <h2 class="post-title">Post Title 2</h2>
+        <p class="post-content">This is the content of the second post. Phasellus fermentum, elit et consectetur vehicula, libero sapien vehicula felis, vel bibendum nulla purus sed orci.</p>
+      </article>
+      
+      <!-- Add more posts as needed -->
+      
+    </main>
+    
+    <footer class="footer">
+      <p>&copy; 2023 Your Blog. All rights reserved.</p>
+    </footer>
+  </div>
     </div>
   );
 }
