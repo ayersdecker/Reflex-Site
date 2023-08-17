7719d500f5603f17bfc1c54a549b4e2eca34222b
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2023-08-17 18:19:16 +0000

    Fixed Styling

--- reflex/src/App.js
+++ reflex/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <div class="container">
     <header class="header">
-      <h1><Clock/></h1>
+      <h2><Clock/></h2>
     </header>
     
     <main>
