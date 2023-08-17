7453332ddad49177e9e156773693cdb257aacf41
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2023-08-17 18:00:26 +0000

    fiexed cc

--- reflex/src/App.js
+++ reflex/src/App.js
@@ -17,35 +17,12 @@ function App() {
     <main>
       <article class="post">
         <h2 class="Time Module">Post Title 1</h2>
-        <p class="post-content"><pr><code>import React, { useState, useEffect } from 'react';
-
-function Clock() {
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime(new Date());
-    }, 1000); // Update every second
-
-    return () => {
-      clearInterval(intervalId); // Clean up the interval when the component unmounts
-    };
-  }, []);
-
-  const formattedTime = time.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true });
-
-  return (
-      <h1 className="time">{formattedTime}</h1>
-
-  );
-}
-
-export default Clock;</code></pr></p>
+        <p class="post-content"><Caption/></p>
       </article>
       
       <article class="post">
-        <h2 class="post-title">Post Title 2</h2>
-        <p class="post-content">This is the content of the second post. Phasellus fermentum, elit et consectetur vehicula, libero sapien vehicula felis, vel bibendum nulla purus sed orci.</p>
+        <h2 class="post-title"><Clock/></h2>
+        <p class="post-content"></p>
       </article>
       
       
