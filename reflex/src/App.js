2b37fc56f6164fe60029db02718b8863ecd16ce6
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2023-08-17 18:16:44 +0000

    Simple Captioning

--- reflex/src/App.js
+++ reflex/src/App.js
@@ -11,25 +11,18 @@ function App() {
     <div className="App">
       <div class="container">
     <header class="header">
-      <h1>Scroller Blog</h1>
+      <h1><Clock/></h1>
     </header>
     
     <main>
       <article class="post">
-        <h2 class="Time Module">Post Title 1</h2>
         <p class="post-content"><Captioning/></p>
       </article>
-      
-      <article class="post">
-        <h2 class="post-title"><Clock/></h2>
-        <p class="post-content"></p>
-      </article>
-      
       
     </main>
     
     <footer class="footer">
-      <p>&copy; 2023 Your Blog. All rights reserved.</p>
+      <p>&copy; 2023 Decker Ayers</p>
     </footer>
   </div>
     </div>
